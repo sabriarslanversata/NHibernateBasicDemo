9bb75ed5948dcf97702b0acfdfe4c2f1ca341f05
Ziyad Abd Afattah <ziyad.abdalfattah@aurea.com>
2018-05-15 22:42:42 +0300

    updated Program class

--- NHibernateBasicDemo/NHibernetDemo/Models/Student.cs
+++ NHibernateBasicDemo/NHibernetDemo/Models/Student.cs
@@ -5,5 +5,10 @@
         public virtual int ID { get; set; }
         public virtual string FirstName { get; set; }
         public virtual string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} {2}", ID, FirstName, LastName);
+        }
     }
 }
--- NHibernateBasicDemo/NHibernetDemo/Program.cs
+++ NHibernateBasicDemo/NHibernetDemo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NHibernetDemo.Models;
+using NHibernetDemo.Services;
 
 namespace NHibernetDemo
 {
@@ -10,6 +8,44 @@ namespace NHibernetDemo
     {
         static void Main(string[] args)
         {
+            var connectionString =
+                "Data Source=.;Initial Catalog=NHibernateDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var studentService = new StudentService(connectionString);
+
+            //var newStudent = studentService.CreateSutdent(new Student
+            //{
+            //    FirstName = "Ziyad",
+            //    LastName = "Abd Alfattah"
+            //});
+
+            //Console.WriteLine(newStudent.ToString());
+
+            //var allStudents = studentService.GetAllStudents();
+            //foreach (var student in allStudents)
+            //{
+            //    Console.WriteLine(student.ToString());
+            //}
+
+            //var studentById = studentService.GetStudentById(1);
+            //Console.WriteLine(studentById);
+
+            //Update Student (id: 1)
+            var studentToUpdate = new Student
+            {
+                ID = 2,
+                FirstName = "Test 1",
+                LastName = "Test 2"
+            };
+            studentService.UpdateStudent(studentToUpdate);
+
+
+            var allStudents = studentService.GetAllStudents();
+            foreach (var student in allStudents)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadLine();
         }
     }
 }
